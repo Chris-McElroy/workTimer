3385c3999be2c45154edfccc66b72836232dd158
ChrisMcElroy <csmcelroy@me.com>
2022-07-13 22:43:59 -0700

    Initial Commit

---
+++ work time/work_timeApp.swift
@@ -0,0 +1,17 @@
+//
+//  work_timeApp.swift
+//  work time
+//
+//  Created by Chris McElroy on 7/13/22.
+//
+
+import SwiftUI
+
+@main
+struct work_timeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
