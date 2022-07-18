c8c21e70b875c77778beff0e7480e943193f9f10
ChrisMcElroy <csmcelroy@me.com>
2022-07-18 16:20:27 -0700

    changed colors, added timer and keep working

--- work time/ContentView.swift
+++ work time/ContentView.swift
@@ -8,12 +8,17 @@
 import SwiftUI
 
 enum CurrentTimer {
-	case none, workTime, breakTime, timerOver
+	case none, workTime, breakTime, workOver, breakOver
 }
+
+let purple = Color(red: 0.518, green: 0.141, blue: 0.667)
+let green = Color(red: 0.027, green: 0.698, blue: 0.667)
 
 struct ContentView: View {
 	@State var currentTimer: CurrentTimer = .none
 	@State var timer: Timer? = nil
+	@State var time: Int = 0
+	@State var startTime: Double = 0
 	
     var body: some View {
 		ZStack {
@@ -22,48 +27,80 @@ struct ContentView: View {
 					withAnimation(.easeIn) {
 						currentTimer = .workTime
 					}
+					startTime = Date.now
+					time = .random(in: 1500..<2700)
 					timer?.invalidate()
-					timer = Timer.scheduledTimer(withTimeInterval: .random(in: 1500..<2700), repeats: false, block: { _ in
-						self.currentTimer = .timerOver
+					timer = Timer.scheduledTimer(withTimeInterval: TimeInterval(time), repeats: false, block: { _ in
+						self.currentTimer = .workOver
 					})
 				}
-				.buttonStyle(MainStyle(color: .blue))
+				.buttonStyle(MainStyle(color: purple))
 				Spacer().frame(height: 100)
 				Button("Start Break") {
 					withAnimation(.easeIn) {
 						currentTimer = .breakTime
 					}
+					startTime = Date.now
+					time = .random(in: 360..<1260)
 					timer?.invalidate()
-					timer = Timer.scheduledTimer(withTimeInterval: .random(in: 360..<1260), repeats: false, block: { _ in
-						self.currentTimer = .timerOver
+					timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { _ in
+						time -= 1
+						if time < 0 {
+							self.currentTimer = .breakOver
+							self.timer?.invalidate()
+						}
 					})
 				}
-				.buttonStyle(MainStyle(color: .orange))
+				.buttonStyle(MainStyle(color: green))
 			}
 			Text("WORK\nTIME!")
 				.font(.system(size: 80, weight: .bold, design: .rounded))
 				.multilineTextAlignment(.center)
 				.frame(width: 400, height: 1000)
-				.background(Rectangle().foregroundColor(.blue))
+				.background(Rectangle().foregroundColor(purple))
 				.foregroundColor(.white)
 				.offset(y: currentTimer == .workTime ? 0 : -1000)
-//				.onTapGesture { self.currentTimer = .timerOver }
-			Text("BREAK\nTIME!")
-				.font(.system(size: 80, weight: .bold, design: .rounded))
-				.multilineTextAlignment(.center)
-				.frame(width: 400, height: 1000)
-				.background(Rectangle().foregroundColor(.orange))
-				.foregroundColor(.white)
-				.offset(y: currentTimer == .breakTime ? 0 : -1000)
-//				.onTapGesture { self.currentTimer = .timerOver }
-			Text("TIMER\nDONE!")
+//				.onTapGesture { self.currentTimer = .workOver }
+			ZStack {
+				Text("BREAK\nTIME!")
+					.font(.system(size: 80, weight: .bold, design: .rounded))
+					.multilineTextAlignment(.center)
+					.frame(width: 400, height: 1000)
+					.background(Rectangle().foregroundColor(green))
+				Text("\(max(time,0)/60):\((max(time,0) % 60)/10)\(max(time,0) % 10)")
+					.font(.system(size: 40, weight: .bold, design: .rounded))
+					.offset(y: -200)
+			}
+			.foregroundColor(.white)
+			.onTapGesture { self.currentTimer = .breakOver }
+			.offset(y: currentTimer == .breakTime ? 0 : -1000)
+			ZStack {
+				Text("WORK\nOVER!")
+					.font(.system(size: 80, weight: .bold, design: .rounded))
+					.multilineTextAlignment(.center)
+					.frame(width: 400, height: 1000)
+					.background(Rectangle().foregroundColor(.red))
+					.foregroundColor(.white)
+					.onTapGesture { self.currentTimer = .none }
+				Button("Keep Working") {
+					currentTimer = .workTime
+					timer?.invalidate()
+					timer = Timer.scheduledTimer(withTimeInterval: 900, repeats: false, block: { _ in
+						self.currentTimer = .workOver
+					})
+				}
+				.buttonStyle(MainStyle(color: purple))
+				.offset(y: 160)
+			}
+			.offset(y: currentTimer == .workOver ? 0 : -1000)
+			Text("BREAK\nOVER!")
 				.font(.system(size: 80, weight: .bold, design: .rounded))
 				.multilineTextAlignment(.center)
 				.frame(width: 400, height: 1000)
 				.background(Rectangle().foregroundColor(.red))
 				.foregroundColor(.white)
 				.onTapGesture { self.currentTimer = .none }
-				.offset(y: currentTimer == .timerOver ? 0 : -1000)
+				.offset(y: currentTimer == .breakOver ? 0 : -1000)
 		}
     }
 }
@@ -82,11 +119,17 @@ struct MainStyle: ButtonStyle {
 	}
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
+
+extension Date {
+	static var now: TimeInterval {
+		timeIntervalSinceReferenceDate
+	}
+	
+	static var ms: Int {
+		Int(timeIntervalSinceReferenceDate*1000)
+	}
 }
+
 
 // Ok so basically what I want is a timer button called “work” that will be a random interval between 25 and 45 minutes
 // And a timer button called “break” with random time between 6 and 21 minutes
